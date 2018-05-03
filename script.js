8886d06cb76d829a33a441983e835289be1b3f90
deusyvan <deusyvan@gmail.com>
2018-05-02 23:17:24 -0300

    Alertando o erro com uma mensagem

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function validar() {
 	var valor = document.getElementById("numero").value;
 	
 	if(valor.length > 2){
+		alert("Você digitou um número com mais de dois algarismos!")
 		return false;
 	} else {
 		true;
